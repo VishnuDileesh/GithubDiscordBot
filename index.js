175f8b167881decf519b0388b5997df8eace06c3
VishnuDileesh <munnadileesh@gmail.com>
2023-09-28 12:06:10 +0530

    Add - update bot to listen for slash commands, and install packages

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ import './keep_alive.js'
 // Discord initialization
 
 import { Client, GatewayIntentBits } from 'discord.js'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v10'
+import { SlashCommandBuilder } from '@discordjs/builders'
 
 // Create a rotating write stream for log files
 
@@ -20,8 +23,6 @@ const accessLogStream = rfs.createStream('access.log', {
   compress: 'gzip' // Compress old log files (optional)
 })
 
-//
-
 // Pino logger initialization
 
 const logger = pino(
@@ -31,10 +32,6 @@ const logger = pino(
   accessLogStream
 )
 
-//
-
-// Discord initialization
-
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -43,7 +40,61 @@ const client = new Client({
   ]
 })
 
-//
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN)
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('issue')
+    .setDescription('To deal with github issues')
+    .addSubcommand(subcommand =>
+      subcommand.setName('create')
+        .setDescription('Create a new issue')
+        .addStringOption(option =>
+          option
+            .setName('project')
+            .setDescription('Project name')
+            .setRequired(true)
+        )
+        .addStringOption(option =>
+          option
+            .setName('title')
+            .setDescription('Issue title')
+            .setRequired(true)
+        )
+        .addStringOption(option =>
+          option
+            .setName('description')
+            .setDescription('Issue description')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('get')
+        .setDescription('Get all issues from project')
+        .addStringOption(option =>
+          option
+            .setName('project')
+            .setDescription('Project name')
+            .setRequired(true)
+        )
+    )
+    .toJSON()
+];
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.')
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
+      { body: commands }
+    )
+
+    console.log('Successfully reloaded application (/) commands.')
+  } catch (error) {
+    console.error(error)
+  }
+})()
 
 // Github initialization
 
@@ -54,13 +105,75 @@ const octokit = new Octokit({
   }
 })
 
-//
-
 // Discord bot code
 
 client.on('ready', () => {
-  logger.logger.info(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
 })
+
+// Creating github issues by listening for slash commands
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return
+
+  const { commandName, options } = interaction
+
+  if (commandName === 'issue') {
+    const subcommand = options.getSubcommand()
+
+    if (subcommand === 'create') {
+      const projectName = options.getString('project')
+      const issueTitle = options.getString('title')
+      const issueDescription = options.getString('description')
+
+      await octokit.rest.issues
+        .create({
+          owner: process.env.GITHUB_USERNAME,
+          repo: projectName,
+          title: issueTitle,
+          body: issueDescription
+        })
+        .then(async ({ data }) => {
+          logger.logger.info(`Issue created in ${projectName} repository with title ${issueTitle}
+        Issue URL: ${data.html_url}`)
+          await interaction.reply(
+          `Issue created in ${projectName} repository with title ${issueTitle}
+          Issue URL: ${data.html_url}
+          `
+          )
+        })
+        .catch((err) => {
+          console.log('err::: ', err)
+          logger.logger.warn(err)
+        })
+    } else if (subcommand === 'get') {
+      const projectName = options.getString('project')
+
+      await octokit.rest.issues.listForRepo({
+        owner: process.env.GITHUB_USERNAME,
+        repo: projectName
+      })
+        .then(async ({ data }) => {
+          console.log('data::: ', data)
+
+          const issues = data.map((issue) => issue.html_url)
+
+          await interaction.reply(
+          `Fetching issues from ${projectName} repository:
+          Done:
+          ${issues.join('\n')}
+          `
+          )
+        })
+        .catch((err) => {
+          console.log('err::: ', err)
+          logger.logger.warn(err)
+        })
+    }
+  }
+})
+
+// Creating github issues by listening to chat message
 
 const prefix = '!'
 
@@ -76,7 +189,7 @@ client.on('messageCreate', async (msg) => {
 
   const args = commands.slice(1)
 
-  logger.logger.info('Args::: ', args)
+  logger.logger.info('args::: ', args)
 
   // !pgb <repo name> <issue name>
 
